d03f71952ed74a8994bcf77f8d9cd2f7a1eb8f6c
munaray <munahdesigns@gmail.com>
2023-08-12 16:41:14 +0100

    models and console.py update

--- console.py
+++ console.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
+"""This defines the HBnB console."""
 import cmd
 import re
 from shlex import split
 from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
+"""Creating a function to tokenize input strings"""
+def tokenize(arg):
+    curly_brackets = re.search(r"\{(.*?)\}", arg)
+    square_brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_brackets is None:
+        if square_brackets is None:
+            return [item.strip(",") for item in split(arg)]
         else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
+            """sbrackets means curly brackets"""
+            before_sbracket = split(arg[:square_brackets.span()[0]])
+            modified_list = [i.strip(",") for i in before_sbracket]
+            modified_list.append(square_brackets.group())
+            return modified_list
     else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
+        before_cbracket = split(arg[:curly_brackets.span()[0]])
+        modified_list = [i.strip(",") for i in before_cbracket]
+        modified_list.append(curly_brackets.group())
+        return modified_list
 
 
 class HBNBCommand(cmd.Cmd):
@@ -87,7 +82,7 @@ class HBNBCommand(cmd.Cmd):
         """Usage: create <class>
         Create a new class instance and print its id.
         """
-        argl = parse(arg)
+        argl = tokenize(arg)
         if len(argl) == 0:
             print("** class name missing **")
         elif argl[0] not in HBNBCommand.__classes:
@@ -100,7 +95,7 @@ class HBNBCommand(cmd.Cmd):
         """Usage: show <class> <id> or <class>.show(<id>)
         Display the string representation of a class instance of a given id.
         """
-        argl = parse(arg)
+        argl = tokenize(arg)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
@@ -116,7 +111,7 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
-        argl = parse(arg)
+        argl = tokenize(arg)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
@@ -134,7 +129,7 @@ class HBNBCommand(cmd.Cmd):
         """Usage: all or all <class> or <class>.all()
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects."""
-        argl = parse(arg)
+        argl = tokenize(arg)
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
@@ -149,7 +144,7 @@ class HBNBCommand(cmd.Cmd):
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()
         Retrieve the number of instances of a given class."""
-        argl = parse(arg)
+        argl = tokenize(arg)
         count = 0
         for obj in storage.all().values():
             if argl[0] == obj.__class__.__name__:
@@ -162,7 +157,7 @@ class HBNBCommand(cmd.Cmd):
        <class>.update(<id>, <dictionary>)
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary."""
-        argl = parse(arg)
+        argl = tokenize(arg)
         objdict = storage.all()
 
         if len(argl) == 0:
