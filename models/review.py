b646bfcdadeaf02dd3deed12f4b237263ace140e
munaray <munahdesigns@gmail.com>
2023-08-13 10:30:23 +0100

    models/

--- models/review.py
+++ models/review.py
@@ -4,13 +4,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
-
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
-    """
+    """Represent a review with the following atrr"""
 
     place_id = ""
     user_id = ""
